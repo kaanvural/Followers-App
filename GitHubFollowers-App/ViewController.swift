5ba03be2cbccc199142bf5ef33487a7533cee0a7
Kaan <>
2020-04-23 11:21:15 +0300

    Project created

---
+++ GitHubFollowers-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubFollowers-App
+//
+//  Created by Kaan on 23.04.2020.
+//  Copyright © 2020 Kaan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

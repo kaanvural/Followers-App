b375d3e4f1554fbbd6019945d8a86dc3854353e9
Kaan <>
2020-04-23 12:30:30 +0300

    Navigation setup done

--- GitHubFollowers-App/FavoritesListVC.swift
+++ GitHubFollowers-App/FavoritesListVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FavoritesListVC.swift
 //  GitHubFollowers-App
 //
 //  Created by Kaan on 23.04.2020.
@@ -8,13 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FavoritesListVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemPink
     }
 
-
 }
-
---
+++ GitHubFollowers-App/SearchVC.swift
@@ -0,0 +1,18 @@
+//
+//  SearchVC.swift
+//  GitHubFollowers-App
+//
+//  Created by Kaan on 23.04.2020.
+//  Copyright © 2020 Kaan. All rights reserved.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+}
